ab0a2a7af2f8da9fc8c9558fd027720335fbfcba
spectralcode <51879191+spectralcode@users.noreply.github.com>
2024-03-31 21:05:18 +0200

    added snapshot save path to persistent settings

--- src/cameraextensionparameters.h
+++ src/cameraextensionparameters.h
@@ -4,17 +4,18 @@
 #include <QString>
 #include <QtGlobal>
 #include <QMetaType>
+#include <QVariant>
 
 #define CAMERA_SELECTION "selected_camera"
 #define CAMERA_ROTATION_ANGLE "camera_rotation_angle"
-
+#define CAMERA_SNAPSHOT_SAVE_PATH "snapshot_save_path"
 
 struct CameraExtensionParameters {
 	QString selectedCamera;
 	qreal rotationAngle;
+	QString snapShotSavePath;
 };
 Q_DECLARE_METATYPE(CameraExtensionParameters)
-
 
 
 #endif // CAMERAEXTENSIONPARAMETERS_H
